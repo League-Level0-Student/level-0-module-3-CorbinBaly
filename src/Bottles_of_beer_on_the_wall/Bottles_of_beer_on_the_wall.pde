d178e5502bd62bb7df45883e4aa5446c55884952
league <league@wts-im-17.attlocal.net>
2018-07-12 20:00:59 -0700

    finished bottles of beer on the wall and fishbuzz

---
+++ src/Bottles_of_beer_on_the_wall/Bottles_of_beer_on_the_wall.pde
@@ -0,0 +1,12 @@
+void setup(){
+  size(500,500);
+}
+void draw(){
+  for (int t = 100; t > 0; t--) {
+      print(" "+ t + " bottles of beer on the wall, take one down pass it around " +t+ " bottles of beer on the wall                                        ");   
+  }
+}
+  
+  
+  
+  
--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -3,6 +3,7 @@
 
 package modulo;
 
+import javax.swing.JOptionPane;
 
 /**
 * Fizz Buzz
@@ -26,6 +27,22 @@ package modulo;
 **/
 
 public class FizzBuzz {
-
+	public static void main(String[] args) {
+		for (int i = 1; i <21; i++) {
+			
+			if(i%5==0 && i%3==0) {
+				JOptionPane.showMessageDialog(null, "FizzBuzz");
+			}
+			else if (i%5==0) {
+				JOptionPane.showMessageDialog(null, "Buzz");
+			}
+			else if(i%3==0) {
+				JOptionPane.showMessageDialog(null,"Fizz");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, i);
+			}
+		}
+	}
 }
 
